6378a337fabd964f282a5c04be3ade288800a3c6
himanshu4507 <himanshuchaudhary4507@gmail.com>
2024-10-24 01:22:08 +0530

    Bottom_nav_bar

--- app/src/main/java/com/example/recipeapp/MainActivity2.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity2.kt
@@ -1,13 +1,16 @@
 package com.example.recipeapp
-
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import HorizontalAdapter
+import VerticalAdaptor
+import android.util.Log
+import android.widget.Toast
 import com.hashdroid.recipeapp.RetrofitClient
 import com.hashdroid.recipeapp.RecipeResponse
+import com.hashdroid.recipeapp.RecipeResponse2
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,18 +18,52 @@ import retrofit2.Response
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var horizontalAdapter: HorizontalAdapter
+    private lateinit var verticalAdapter: VerticalAdaptor
 
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
         // Initialize RecyclerView
         val recyclerView1 = findViewById<RecyclerView>(R.id.recycler_view1)
+        val recyclerView2= findViewById<RecyclerView>(R.id.recycler_view2)
+
         recyclerView1.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView2.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         // Fetch random recipes
+        fetchALLRecipes()
         fetchRandomRecipes()
+
+    }
+
+    private fun fetchALLRecipes() {
+        val apiKey = "195f87d5a199467797f27b34555430e1"
+        val retrofit = RetrofitClient.retrofit
+        val call = retrofit.getSearchRecipes( 100,apiKey)
+        call.enqueue(object : Callback<RecipeResponse2> {
+            override fun onResponse(call: Call<RecipeResponse2>, response: Response<RecipeResponse2>) {
+                if (response.isSuccessful) {
+
+                    Log.d("Ashu", response.body().toString())
+                    response.body()?.results?.let { results ->
+                        verticalAdapter = VerticalAdaptor(results)
+                        findViewById<RecyclerView>(R.id.recycler_view2).adapter = verticalAdapter
+                    }
+                }
+                else{
+                    Toast.makeText(this@MainActivity2, "Error: ${response.code()}", Toast.LENGTH_LONG).show()
+
+                }
+            }
+
+
+
+            override fun onFailure(call: Call<RecipeResponse2>, t: Throwable) {
+                Toast.makeText(this@MainActivity2, "Error", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 
     private fun fetchRandomRecipes() {
@@ -49,3 +86,7 @@ class MainActivity2 : AppCompatActivity() {
         })
     }
 }
+
+
+
+
